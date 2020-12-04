e1a128ef74c50017abb73a2737f353fce63fcf44
unknown <ifteher.alom@gmail.com>
2020-12-04 15:11:48 +0600

    dapp

--- fabric-handler.js
+++ fabric-handler.js
@@ -1,12 +1,4 @@
 'use strict';
-/*
-* Copyright IBM Corp All Rights Reserved
-*
-* SPDX-License-Identifier: Apache-2.0
-*/
-/*
- * Chaincode Invoke
- */
 
 var Fabric_Client = require('fabric-client');
 var path = require('path');
@@ -18,7 +10,7 @@ var fabric_client = new Fabric_Client();
 
 // setup the fabric network
 var channel = fabric_client.newChannel('mychannel');
-var peer = fabric_client.newPeer('grpc://localhost:8051');
+var peer = fabric_client.newPeer('grpc://localhost:7051');
 channel.addPeer(peer);
 var order = fabric_client.newOrderer('grpc://localhost:7050')
 channel.addOrderer(order);
@@ -35,11 +27,16 @@ function submitTransaction() {
     return new Promise((resolve, reject) => {
         Fabric_Client.newDefaultKeyValueStore({ path: store_path
         }).then((state_store) => {
+            // assign the store to the fabric client
             fabric_client.setStateStore(state_store);
             var crypto_suite = Fabric_Client.newCryptoSuite();
+            // use the same location for the state store (where the users' certificate are kept)
+            // and the crypto store (where the users' keys are kept)
             var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
             crypto_suite.setCryptoKeyStore(crypto_store);
             fabric_client.setCryptoSuite(crypto_suite);
+        
+            // get the enrolled user from persistence, this user will sign all requests
             return fabric_client.getUserContext('user1', true);
         }).then((user_from_store) => {
             if (user_from_store && user_from_store.isEnrolled()) {
@@ -48,27 +45,24 @@ function submitTransaction() {
             } else {
                 throw new Error('Failed to get user1.... run registerUser.js');
             }
+        
+            // get a transaction id object based on the current user assigned to fabric client
             tx_id = fabric_client.newTransactionID();
             console.log("Assigning transaction_id: ", tx_id._transaction_id);
-            
-            // var request = {
-            //     //targets: let default to the peer assigned to the client
-            //     chaincodeId: 'fabcar',
-            //     fcn: 'storeCode',
-            //     args: ['TC1', 'SP1', 'IDP1', '1122', '3344', 'pending', 'pending'],
-            //     chainId: 'mychannel',
-            //     txId: tx_id
-            // };
-
+        
+            // createCar chaincode function - requires 5 args, ex: args: ['CAR12', 'Honda', 'Accord', 'Black', 'Tom'],
+            // changeCarOwner chaincode function - requires 2 args , ex: args: ['CAR10', 'Dave'],
+            // must send the proposal to endorsing peers
             var request = {
                 //targets: let default to the peer assigned to the client
                 chaincodeId: 'fabcar',
-                fcn: 'createCar',
-                args: ['CAR11', 'Chevy', 'Volt', 'Red', 'Nick'],
+                fcn: 'queryAllCars',
+                args: [''],
                 chainId: 'mychannel',
                 txId: tx_id
             };
         
+            // send the transaction proposal to the peers
             return channel.sendTransactionProposal(request);
         }).then((results) => {
             var proposalResponses = results[0];
@@ -92,13 +86,22 @@ function submitTransaction() {
                     proposal: proposal
                 };
         
+                // set the transaction listener and set a timeout of 30 sec
+                // if the transaction did not get committed within the timeout period,
+                // report a TIMEOUT status
                 var transaction_id_string = tx_id.getTransactionID(); //Get the transaction ID string to be used by the event processing
                 var promises = [];
         
                 var sendPromise = channel.sendTransaction(request);
-                Promise.resolve(sendPromise)
+                promises.push(sendPromise); //we want the send transaction first, so that we know where to check status
+        
+                // get an eventhub once the fabric client has a user assigned. The user
+                // is required bacause the event registration must be signed
                 let event_hub = channel.newChannelEventHub(peer);
         
+                // using resolve the promise so that result status may be processed
+                // under the then clause rather than having the catch clause process
+                // the status
                 let txPromise = new Promise((resolve, reject) => {
                     let handle = setTimeout(() => {
                         event_hub.unregisterTxEvent(transaction_id_string);
@@ -106,8 +109,11 @@ function submitTransaction() {
                         resolve({event_status : 'TIMEOUT'}); //we could use reject(new Error('Trnasaction did not complete within 30 seconds'));
                     }, 3000);
                     event_hub.registerTxEvent(transaction_id_string, (tx, code) => {
+                        // this is the callback for transaction event status
+                        // first some clean up of event listener
                         clearTimeout(handle);
         
+                        // now let the application know what happened
                         var return_status = {event_status : code, tx_id : transaction_id_string};
                         if (code !== 'VALID') {
                             console.error('The transaction was invalid, code = ' + code);
@@ -117,6 +123,7 @@ function submitTransaction() {
                             resolve(return_status);
                         }
                     }, (err) => {
+                        //this is the callback if something goes wrong with the event registration or processing
                         reject(new Error('There was a problem with the eventhub ::'+err));
                     },
                         {disconnect: true} //disconnect when complete
@@ -145,10 +152,11 @@ function submitTransaction() {
             } else {
                 console.log('Transaction failed to be committed to the ledger due to ::'+results[1].event_status);
             }
+
             resolve()
         }).catch((err) => {
             console.error('Failed to invoke successfully :: ' + err);
-            resolve()
+            resolve();
         });
     });
 }
